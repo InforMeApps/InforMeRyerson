b2e8126c7b1724566cb57dc31c2bb0e14756b101
Tanmay Parikh <tanpar1995@gmail.com>
2014-08-16 19:00:48 -0400

    Added header to list

--- app/src/main/java/ca/informeapps/informeryerson/CampusLife/Schedule/ScheduleActivity.java
+++ app/src/main/java/ca/informeapps/informeryerson/CampusLife/Schedule/ScheduleActivity.java
@@ -4,17 +4,23 @@
 
 package ca.informeapps.informeryerson.CampusLife.Schedule;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import ca.informeapps.informeryerson.R;
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 
 /**
@@ -22,11 +28,7 @@ import ca.informeapps.informeryerson.R;
  */
 public class ScheduleActivity extends FragmentActivity {
 
-    public Calendar[] Day;
-    public String[] Days;
-    public String[] MonthDays;
-    private ListView listView;
-    private String[] calendarID;
+    private StickyListHeadersListView listView;
     private long[] timeMills;
 
     @Override
@@ -34,95 +36,28 @@ public class ScheduleActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myschedule);
 
-
-        Day = new Calendar[180];
-        Days = new String[180];
-        MonthDays = new String[180];
         timeMills = new long[180];
 
-
         for (int z = 0; z < 180; z++) {
-            Day[z] = shiftedCalender(Calendar.getInstance(), z);
-            Days[z] = shiftDate(Calendar.getInstance(), z);
-            MonthDays[z] = whatMonth(Day[z]);
+
             timeMills[z] = shiftedCalender(Calendar.getInstance(), z).getTimeInMillis();
         }
 
 
-        /*
-        //
-        // GETTING STUFF FROM CALENDAR START
-        //
-        ContentResolver contentResolver = getContentResolver();
-        final Cursor cursor = contentResolver.query(CalendarContract.Calendars.CONTENT_URI,
-                (new String[]{CalendarContract.Calendars._ID, CalendarContract.Calendars.CALENDAR_DISPLAY_NAME}), null, null, null);
-
-        while (cursor.moveToNext()) {
-            final String _id = cursor.getString(0);
-            final String displayName = cursor.getString(1);
-
-
-            int stringLength = displayName.length();
-            String output = displayName.substring(stringLength - 10);
-            //Log.d("Cursor", output);
-
-            if (output.equals("ryerson.ca")) {
-                //Log.d("Cursor", "true");
-                calendarID = new String[]{_id};
-            }
-        }
-
-        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
-
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2014, Calendar.SEPTEMBER, 2, 8, 0);
-        long startMills = beginTime.getTimeInMillis();
-
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2014, Calendar.SEPTEMBER, 2, 20, 0);
-        long endMills = endTime.getTimeInMillis();
-
-        ContentUris.appendId(builder, startMills);
-        ContentUris.appendId(builder, endMills);
-
-        Cursor eventCursor = contentResolver.query(builder.build(), new String[]{CalendarContract.Instances.TITLE,
-                        CalendarContract.Instances.BEGIN, CalendarContract.Instances.END, CalendarContract.Instances.DESCRIPTION},
-                CalendarContract.Instances.CALENDAR_ID + " = ?", calendarID, null);
-
-        while (eventCursor.moveToNext()) {
-            final String title = eventCursor.getString(0);
-            final Date begin = new Date(eventCursor.getLong(1));
-            final Date end = new Date(eventCursor.getLong(2));
-            final String description = eventCursor.getString(3);
-
-            Log.d("Cursor", "Title: " + title + "\tDescription: " + description + "\tBegin: " + begin + "\tEnd: " + end);
-        }
-
-        //
-        // GETTING STUFF FROM CALENDAR END
-        //
-        */
-
-
-        final ScheduleListDateAdapter adapter = new ScheduleListDateAdapter(this, Days, MonthDays);
-        listView = (ListView) findViewById(R.id.listview_myschedule_date);
-
-
+        ScheduleDateListAdapter adapter = new ScheduleDateListAdapter(this);
+        listView = (StickyListHeadersListView) findViewById(R.id.listview_myschedule_date);
         listView.setAdapter(adapter);
-
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 onItemSelection(i);
+                Log.d("CLICK", "CLICK");
             }
-
         });
 
         onItemSelection(0);
 
     }
-
 
     public Calendar shiftedCalender(Calendar c, int Shift) {
         Calendar calendar = c;
@@ -131,19 +66,6 @@ public class ScheduleActivity extends FragmentActivity {
         calendar.add(Calendar.DAY_OF_YEAR, Shift);
         return calendar;
     }
-
-    public String shiftDate(Calendar c, int Shift) {
-        Calendar calendar = c;
-        calendar.add(Calendar.DAY_OF_MONTH, Shift);
-        return (new SimpleDateFormat("EEE").format(calendar.getTime()));
-    }
-
-    public String whatMonth(Calendar c) {
-        return (new SimpleDateFormat("MMM, d").format(c.getTime()));
-
-
-    }
-
 
     public void onItemSelection(int Pos) {
         Bundle bundle = new Bundle();
@@ -156,4 +78,113 @@ public class ScheduleActivity extends FragmentActivity {
         getFragmentManager().beginTransaction().replace(R.id.content_frame_myschedule, fragInfo).commit();
 
     }
+
+    class ScheduleDateListAdapter extends BaseAdapter implements StickyListHeadersAdapter {
+
+        private String[] Dates;
+        private String[] monthHeader;
+        private LayoutInflater inflater;
+
+        public ScheduleDateListAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+            Dates = new String[180];
+            monthHeader = new String[180];
+            for (int x = 0; x < Dates.length; x++) {
+                Dates[x] = Day(shiftedCalender(Calendar.getInstance(), x))[1] + ", " + Day(shiftedCalender(Calendar.getInstance(), x))[2];
+                monthHeader[x] = (shiftedCalender(Calendar.getInstance(), x));
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return Dates.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return Dates[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder dayTextHolder;
+            ViewHolder dateTextHolder;
+
+            if (convertView == null) {
+                dayTextHolder = new ViewHolder();
+                dateTextHolder = new ViewHolder();
+
+                convertView = inflater.inflate(R.layout.layout_list_myschedule, parent, false);
+
+                dayTextHolder.text = (TextView) convertView.findViewById(R.id.TextView_MonthName_MMM);
+                dateTextHolder.text = (TextView) convertView.findViewById(R.id.TextView_DateNumber);
+
+                convertView.setTag(dayTextHolder);
+                convertView.setTag(dateTextHolder);
+            } else {
+                dayTextHolder = (ViewHolder) convertView.getTag();
+                dateTextHolder = (ViewHolder) convertView.getTag();
+            }
+
+            String dayText = Dates[position].substring(0, 3).toUpperCase();
+            dayTextHolder.text.setText(dayText);
+
+            String dateText = Dates[position].substring(Dates[position].length() - 2);
+            dateTextHolder.text.setText(dateText);
+
+            return convertView;
+        }
+
+        @Override
+        public View getHeaderView(int position, View convertView, ViewGroup parent) {
+            HeaderViewHolder holder;
+            if (convertView == null) {
+                holder = new HeaderViewHolder();
+                convertView = inflater.inflate(R.layout.layout_list_schedule_datepicker_stickyheader, parent, false);
+                holder.text = (TextView) convertView.findViewById(R.id.textview_schedule_list_header);
+                convertView.setTag(holder);
+            } else {
+                holder = (HeaderViewHolder) convertView.getTag();
+            }
+            //set header text as first char in name
+            String headerText = "" + Day(monthHeader[position])[0];
+            holder.text.setText(headerText);
+            return convertView;
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            //return the first character of the country as ID because this is what headers are based upon
+            return monthHeader[position].subSequence(0, 1).charAt(0);
+        }
+
+        public String shiftedCalender(Calendar c, int Shift) {
+            Calendar calendar = c;
+            calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+            calendar.add(Calendar.DAY_OF_YEAR, Shift);
+            return (new SimpleDateFormat("MMMM EEEE dd").format(calendar.getTime()));
+        }
+
+        public String[] Day(String s) {
+            String p[];
+            p = s.split(" ");
+            return p;
+        }
+    }
+
+    class HeaderViewHolder {
+        TextView text;
+    }
+
+    class ViewHolder {
+        TextView text;
+    }
+
 }
