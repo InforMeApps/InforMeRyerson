626bd5884856b78b34d84afc960ebedc2bb5b10f
TheCanadainBrownie <shahar.amin7@gmail.com>
2014-08-23 22:23:12 -0700

    Added Scrolling to the front tutorial No Proguard 2.0

---
+++ app/src/main/java/ca/informeapps/informeryerson/CustomViewPager.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014. Permission to use, copy, modify, distribute and sell this software and its documentation for any purpose is not granted without permission from owner, for more information please contact informeapplications@gmail.com
+ */
+
+package ca.informeapps.informeryerson;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.animation.Interpolator;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Shahar on 2014-08-23.
+ */
+
+
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(Context context) {
+        super(context);
+        postInitViewPager();
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        postInitViewPager();
+    }
+
+    private ScrollerCustomDuration mScroller = null;
+
+    /**
+     * Override the Scroller instance with our own class so we can change the
+     * duration
+     */
+    private void postInitViewPager() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            Field interpolator = viewpager.getDeclaredField("sInterpolator");
+            interpolator.setAccessible(true);
+
+            mScroller = new ScrollerCustomDuration(getContext(),
+                    (Interpolator) interpolator.get(null));
+            scroller.set(this, mScroller);
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * Set the factor by which the duration will change
+     */
+    public void setScrollDurationFactor(double scrollFactor) {
+        mScroller.setScrollDurationFactor(scrollFactor);
+    }
+
+}
