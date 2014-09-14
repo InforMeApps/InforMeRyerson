fd7b4812c19ad848632bfc3976c2209d68625486
TheCanadainBrownie <shahar.amin7@gmail.com>
2014-09-14 01:15:16 -0700

    Added reminder edit screen Making reminder more user friendly Code cleaning Add A Edit Drawable Forgot to add the classes

---
+++ app/src/main/java/ca/informeapps/informeryerson/Notifications.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014. Permission to use, copy, modify, distribute and sell this software and its documentation for any purpose is not granted without permission from owner, for more information please contact informeapplications@gmail.com
+ */
+
+package ca.informeapps.informeryerson;
+
+/**
+ * Created by Shahar on 2014-09-14.
+ */
+public class Notifications {
+
+    Notifications()
+    {
+
+    }
+}
